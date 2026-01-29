69f9f25c82f9d413470d21be9a1828a44833343d
MrAlderson99 <lucieltonmanoel@gmail.com>
2026-01-29 14:15:41 -0300

    Setup for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: '/portfolio-2025/',
->>>>>>> 446a57a (Setup for Github Pages)
 });
